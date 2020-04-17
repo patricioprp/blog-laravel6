7aeda8c517f60f85d12a981bdc7146ade3ad7a00
Ezequiel <ezequielszurita@gmail.com>
2020-04-17 16:23:43 -0300

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+#Route::get('articles', function () {
+#    echo 'Esta es la seccion de articulos';
+#});
+
+#Route::get('articles/{name?}', function ($name = "Invitado") {
+#    echo 'Bienvenido ' .$name;
+#});
+
+Route::group(['prefix' => 'articles'],function () {
+
+    Route::get('view/{article?}', function ($article = "Vacio") {
+        echo $article;
+    });
+});
