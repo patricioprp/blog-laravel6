b1ef710d5745d1df24733cac78239245e0334cc3
Ezequiel <ezequielszurita@gmail.com>
2020-04-17 15:41:07 -0300

    Relaciones

--- app/Article.php
+++ app/Article.php
@@ -9,4 +9,19 @@ class Article extends Model
     protected $table = "articles";
 
     protected $fillable = ['title','content', 'category_id','user_id'];
+
+    public function category(){  //establece la relacion entre modelos, en este caso muchos a 1
+        return $this -> belongsTo('App\Category');
+    }
+    public function user(){
+        return $this -> belongsTo('App\User');
+    }
+
+    public function images(){
+        return $this -> hasMany('App\Image');
+    }
+
+    public function  tags(){
+        return $this -> belongsToMany('App\Tag');
+    }
 }
--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,8 @@ class Category extends Model
     protected $table = "categories";
 
     protected $fillable = ['name']; //que me gustaria mostrar de la migracion o bbdd
+
+    public function articles(){  //establece la relacion entre modelos, en este caso 1 a muchos
+        return $this -> hasMany('App\Articles');
+    }
 }
