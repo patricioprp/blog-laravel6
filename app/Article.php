3e3f25f4c55f5ca234356c20bb2ffdc706d8c58c
Ezequiel <ezequielszurita@gmail.com>
2020-04-17 21:55:05 -0300

    Instalación de paquetes con composer - Para la nueva version todo cambio:

--- app/Article.php
+++ app/Article.php
@@ -2,10 +2,29 @@
 
 namespace App;
 
+use Cocur\Slugify\SlugifyInterface;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Article extends Model
 {
+
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
     protected $table = "articles";
 
     protected $fillable = ['title','content', 'category_id','user_id'];
